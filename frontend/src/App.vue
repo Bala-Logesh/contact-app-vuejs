a03f34134bd17c26f6f5e7d542dc469b78d79903
Bala Logesh S <balassmp@gmail.com>
2025-02-21 00:55:56 -0500

    Added header component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
+<template>
+  <div class="w-screen h-screen bg-amber-50">
+    <Header title="Contact App" />
+    <RouterView />
+  </div>
+</template>
+
 <script setup>
 import { RouterView } from 'vue-router'
+import Header from './components/Header.vue'
 </script>
-
-<template>
-  <RouterView />
-</template>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="flex flex-row justify-between items-center p-6 md:px-32 lg:px-64 bg-blue-950 text-white">
+        <router-link class="text-3xl font-semibold cursor-pointer" to="/">{{ title }}</router-link>
+        <router-link class="text-xl hover:underline cursor-pointer" to="create-contact">Create Contact</router-link>
+    </nav>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String,
+    required: true
+})
+</script>
